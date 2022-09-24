cc5e1607d79f3481299b0ae26d65fafc9095d23e
ArtemPER0V <98822741+ArtemPER0V@users.noreply.github.com>
2022-09-24 11:31:12 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const d = new Date();
+let day = d.getDay();
+if(day >= 6) {
+    document.getElementById("EJournal").setAttribute("href", "https://lyceum7.eljur.ru/journal-app/u.1804/week.-1");
+} 
